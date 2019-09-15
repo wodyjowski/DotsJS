4fff5166b3ac722c2b3f5b041155fe0dc69f4042
Michał Kisiel <mkisiel58@gmail.com>
2019-09-15 23:03:53 +0200

    included nodejs server

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+var bodyParser = require('body-parser');
+
+var mustacheExpress = require('mustache-express');
+const app = express();
+
+app.engine('html', mustacheExpress());
+app.use(express.static('public'))
+app.set('view engine', 'html');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+const port = process.env.PORT || 80;
+
+
+app.get('/', (req, res) => {
+    res.render('index')
+});
+
