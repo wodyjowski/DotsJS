dec52fa7d37a822db0226a156a3c2df1ce7847fe
Michał Kisiel <mkisiel58@gmail.com>
2019-09-15 22:59:01 +0200

    better stars

--- script.js
+++ script.js
@@ -1,26 +1,42 @@
 'use strict';
 
-const canvas = document.getElementById('canvas');
+let maxStars = 500;
+const stars = []
 
+const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+
+
 
 window.requestAnimationFrame(draw);
 
-let i = 0;
-
-const stars = []
+function initStars()
+{
+    
+}
 
 
 class Star {
     constructor(x, y) {
       this.x = x;
       this.y = y;
+      this.size = getRandomFloat(0.5, 5);
+      this.speed = getRandomFloat(0.1, 1);
+
     }
 }
 
-for (let index = 0; index < 20; index++) {
-    stars.push(new Star(getRandomX(),getRandomY()));
+function addStars()
+{
+    if(stars.length >= maxStars) return;
+
+    let random = getRandomInt(0, 10);
+    
+    for (let index = 0; index < random; index++) {
+        stars.push(new Star(getRandomX(), 0));
+    }
 }
+
 
 function getRandomX() {
     return getRandomInt(0, window.innerWidth);
@@ -36,6 +52,10 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
+function getRandomFloat(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
 function draw() {
     canvas.width  = window.innerWidth;
     canvas.height = window.innerHeight;
@@ -44,15 +64,39 @@ function draw() {
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 
-    ctx.fillStyle = "#FF0000";
+    addStars();
+
+    ctx.fillStyle = "#FFFFFF";
     for (let index = 0; index < stars.length; index++) {
         const element = stars[index];
 
-        ctx.fillRect(element.x, element.y, 4, 4);
+        ctx.fillRect(element.x, element.y, element.size, element.size);
     }
 
-
+    moveStars();
 
 
     window.requestAnimationFrame(draw);
+}
+
+function moveStars()
+{
+    const center = {
+        x: window.innerWidth/2,
+        y: window.innerHeight/2
+    };
+
+    for (let index = 0; index < stars.length; index++) {
+        const element = stars[index];
+        
+
+
+            element.y += element.speed;
+   
+
+        if(element.y >= window.innerHeight)
+        {
+            stars.splice(stars.indexOf(element),1);
+        }
+    }
 }
